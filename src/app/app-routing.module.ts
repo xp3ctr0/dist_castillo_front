6a755130fdaf195812d44ef1eefb3de6ebd18c79
Jhon Gonzalez <jegc9323@gmail.com>
2023-03-03 11:24:28 -0500

    push inicial (jegc)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./Login/login.component";
+import {EntradasComponent} from "./Entradas/entradas.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'entradas', component: EntradasComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Login/login.component';
+import { EntradasComponent } from './Entradas/entradas.component';
+import { LayoutComponent } from './Layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EntradasComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
